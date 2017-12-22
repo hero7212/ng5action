ca69e102acb00a504a557fc2e0a34059c80b4fdd
hero7212 <2423467330@qq.com>
2017-12-22 17:48:32 +0800

    action

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProductComponent } from './product/product.component';
+import { StarsComponent } from './stars/stars.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ 
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SearchComponent,
+    CarouselComponent,
+    ProductComponent,
+    StarsComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ 
+    BrowserModule,
+    
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],  
+  bootstrap: [AppComponent]  
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
+//import './polyfills.ts'   //导入这个是为了兼容一些ie浏览器 
+
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'; //导入使用哪个模块来启动应用
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
+if (environment.production) {   //判断生产环境
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule)   //启动应用
+  .catch(err => console.log(err));    
